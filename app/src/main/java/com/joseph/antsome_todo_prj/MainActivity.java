ae1f98f65c2643896b4a07eaf2951e5311181b8d
Joseph YOO <chelsealoveyu@gmail.com>
2020-04-09 15:24:57 +0900

    First commit

---
+++ app/src/main/java/com/joseph/antsome_todo_prj/MainActivity.java
@@ -0,0 +1,14 @@
+package com.joseph.antsome_todo_prj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
